eee42d886c22c583d4c51e24fe2762de3a226763
peunsu <44690893+peunsu@users.noreply.github.com>
2024-01-02 14:57:26 +0000

    Fix the .snbt file is not properly parsed. Add Google translation function.

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 import os
+import glob
 import re
 import json
+from googletrans import Translator
 
-TITLE_REGEX = re.compile(r'(?<=\btitle: )\"[\s\S.]*?\"', flags=re.MULTILINE)
-SUBTITLE_REGEX = re.compile(r'(?<=\bsubtitle: )\"[\s\S.]*?\"', flags=re.MULTILINE)
+TITLE_REGEX = re.compile(r'(?<=\btitle: )\"(?:[^"\\]|\\.)*\"', flags=re.MULTILINE)
+SUBTITLE_REGEX = re.compile(r'(?<=\bsubtitle: )\"(?:[^"\\]|\\.)*\"', flags=re.MULTILINE)
 DESC_REGEX = re.compile(r'(?<=\bdescription: )\[[\s\S.]*?\]\s', flags=re.MULTILINE)
 IMAGE_REGEX = re.compile(r'{image:[0-9a-zA-Z]*:.*}', flags=re.MULTILINE)
 COLOR_CODE_REGEX = re.compile(r'&[0-9A-FK-OR]', flags=re.IGNORECASE | re.MULTILINE)
 STRING_REGEX = re.compile(r'(?<=\")(?:(?=\\?)\\?.)*?(?=\")', flags=re.MULTILINE)
 
-QUEST_PATH = "/workspaces/ftbq-localization-tool/quests/"
-OUTPUT_PATH = "/workspaces/ftbq-localization-tool/output/"
+QUEST_PATH = "./quests/"
+OUTPUT_QUEST_PATH = "./output/quests/"
+OUTPUT_LANG_PATH = "./output/lang/"
 
 MODPACK_NAME = "atm9"
 
-lang_en_us = dict()
+lang_dict = dict()
 
 def replace_snbt(snbt: str, modpack: str, chapter: str) -> str:
     for key in ["description", "subtitle", "title"]:
@@ -27,29 +30,25 @@ def replace_snbt(snbt: str, modpack: str, chapter: str) -> str:
         
         for i, element in enumerate(regex.findall(snbt)):
             for j, el in enumerate(filter(lambda x: x, STRING_REGEX.findall(element))):
-                if el.startswith("["):
-                    print(key)
-                    print(element, end="\n\n")
-                    print(el)
                 snbt = snbt.replace(el, f"{{{modpack}.{chapter}.{key}.{i}.{j}}}", 1)
-                lang_en_us[f"{modpack}.{chapter}.{key}.{i}.{j}"] = el
+                lang_dict[f"{modpack}.{chapter}.{key}.{i}.{j}"] = el
                 
     return snbt
 
-if not os.path.exists(os.path.join(OUTPUT_PATH, "chapters")):
-    os.mkdir(os.path.join(OUTPUT_PATH, "chapters"))
+os.makedirs(os.path.join(OUTPUT_QUEST_PATH, "chapters"), exist_ok=True)
+os.makedirs(OUTPUT_LANG_PATH, exist_ok=True)
 
-with open(os.path.join(QUEST_PATH, "chapter_groups.snbt"), "r") as f:
-    snbt = f.read()
-snbt = replace_snbt(snbt, modpack=MODPACK_NAME, chapter="chapter_groups")
-with open(os.path.join(OUTPUT_PATH, "chapter_groups.snbt"), "w") as f:
-    f.write(snbt)
-
-for filename in os.listdir(os.path.join(QUEST_PATH, "chapters")):
-    with open(os.path.join(QUEST_PATH, "chapters", filename), "r") as f:
+file_list = [os.path.join(QUEST_PATH, "chapter_groups.snbt"), *glob.glob(os.path.join(QUEST_PATH, "chapters/*.snbt"))]
+for file_path in file_list:
+    with open(file_path, "r", encoding="utf-8-sig") as f:
         snbt = f.read()
-    snbt = replace_snbt(snbt, modpack=MODPACK_NAME, chapter=filename[:-5])
-    with open(os.path.join(OUTPUT_PATH, "chapters", filename), "w") as f:
+    snbt = replace_snbt(snbt, modpack=MODPACK_NAME, chapter=os.path.basename(file_path)[:-5])
+    with open(file_path.replace(QUEST_PATH, os.path.join(OUTPUT_QUEST_PATH), 1), "w", encoding="utf-8-sig") as f:
         f.write(snbt)
 
-json.dump(lang_en_us, open(os.path.join(OUTPUT_PATH, "en_us.json"), "w"), indent=4)
+json.dump(lang_dict, open(os.path.join(OUTPUT_LANG_PATH, "en_us.json"), "w"), indent=4, ensure_ascii=False)
+
+translator = Translator()
+results = translator.translate(list(lang_dict.values()), dest="ko")
+ko_lang_dict = {k: v for k, v in zip(lang_dict.keys(), map(lambda x: x.text, results))}
+json.dump(ko_lang_dict, open(os.path.join(OUTPUT_LANG_PATH, "ko_kr.json"), "w"), indent=4, ensure_ascii=False)
