5b70c910e755c247314cfe6852c846709a7e0e1f
peunsu <44690893+peunsu@users.noreply.github.com>
2024-01-04 14:56:55 +0000

    Fix some strings in .snbt file is not properly substituted.

--- app.py
+++ app.py
@@ -1,25 +1,33 @@
-import os
-import glob
 import re
 import json
+import shutil
+
+from pathlib import Path
 from googletrans import Translator
 
 TITLE_REGEX = re.compile(r'(?<=\btitle: )\"(?:[^"\\]|\\.)*\"', flags=re.MULTILINE)
 SUBTITLE_REGEX = re.compile(r'(?<=\bsubtitle: )\"(?:[^"\\]|\\.)*\"', flags=re.MULTILINE)
 DESC_REGEX = re.compile(r'(?<=\bdescription: )\[[\s\S.]*?\]\s', flags=re.MULTILINE)
 IMAGE_REGEX = re.compile(r'{image:[0-9a-zA-Z]*:.*}', flags=re.MULTILINE)
-COLOR_CODE_REGEX = re.compile(r'&[0-9A-FK-OR]', flags=re.IGNORECASE | re.MULTILINE)
 STRING_REGEX = re.compile(r'(?<=\")(?:(?=\\?)\\?.)*?(?=\")', flags=re.MULTILINE)
 
-QUEST_PATH = "./quests/"
-OUTPUT_QUEST_PATH = "./output/quests/"
-OUTPUT_LANG_PATH = "./output/lang/"
+QUEST_PATH = Path("./quests")
+OUTPUT_PATH = Path("./output")
+OUTPUT_QUEST_PATH = OUTPUT_PATH / "quests"
+OUTPUT_LANG_PATH = OUTPUT_PATH / "lang"
 
 MODPACK_NAME = "atm9"
 
 lang_dict = dict()
 
-def replace_snbt(snbt: str, modpack: str, chapter: str) -> str:
+def convert_snbt(snbt: str, modpack: str, chapter: str) -> str:
+    def filter_string(string: str) -> bool:
+        if not string:
+            return False
+        if string[0] == "{" and string[-1] == "}":
+            return False
+        return True
+    
     for key in ["description", "subtitle", "title"]:
         if key == "title":
             regex = TITLE_REGEX
@@ -29,26 +37,46 @@ def replace_snbt(snbt: str, modpack: str, chapter: str) -> str:
             regex = DESC_REGEX
         
         for i, element in enumerate(regex.findall(snbt)):
-            for j, el in enumerate(filter(lambda x: x, STRING_REGEX.findall(element))):
-                snbt = snbt.replace(el, f"{{{modpack}.{chapter}.{key}.{i}.{j}}}", 1)
+            for j, el in enumerate(filter(filter_string, STRING_REGEX.findall(element))):
+                snbt = snbt.replace(f'"{el}"', f'"{{{modpack}.{chapter}.{key}.{i}.{j}}}"')
                 lang_dict[f"{modpack}.{chapter}.{key}.{i}.{j}"] = el
-                
     return snbt
 
-os.makedirs(os.path.join(OUTPUT_QUEST_PATH, "chapters"), exist_ok=True)
-os.makedirs(OUTPUT_LANG_PATH, exist_ok=True)
+def translate_lang(lang_dict: dict, dest: str) -> dict:
+    translator = Translator()
+    for k, v in lang_dict.items():
+        lang_dict[k] = translator.translate(v, dest=dest).text
+    return lang_dict
 
-file_list = [os.path.join(QUEST_PATH, "chapter_groups.snbt"), *glob.glob(os.path.join(QUEST_PATH, "chapters/*.snbt"))]
-for file_path in file_list:
-    with open(file_path, "r", encoding="utf-8-sig") as f:
-        snbt = f.read()
-    snbt = replace_snbt(snbt, modpack=MODPACK_NAME, chapter=os.path.basename(file_path)[:-5])
-    with open(file_path.replace(QUEST_PATH, os.path.join(OUTPUT_QUEST_PATH), 1), "w", encoding="utf-8-sig") as f:
-        f.write(snbt)
+def save_lang(lang_dict: dict, lang: str="en_us") -> None:
+    OUTPUT_LANG_PATH.mkdir(parents=True, exist_ok=True)
+    json.dump(lang_dict, open(OUTPUT_LANG_PATH/f"{lang}.json", "w", encoding="utf-8-sig"), indent=4, ensure_ascii=False)
 
-json.dump(lang_dict, open(os.path.join(OUTPUT_LANG_PATH, "en_us.json"), "w"), indent=4, ensure_ascii=False)
+def copy_quests() -> None:
+    try:
+        shutil.copytree(QUEST_PATH, OUTPUT_QUEST_PATH)
+    except FileExistsError:
+        check = input("output/quests already exists. Do you want to delete it? (y/n): ")
+        if check == "y":
+            shutil.rmtree(OUTPUT_QUEST_PATH)
+            shutil.copytree(QUEST_PATH, OUTPUT_QUEST_PATH)
+        else:
+            print("Aborted.")
+            exit(1)
 
-translator = Translator()
-results = translator.translate(list(lang_dict.values()), dest="ko")
-ko_lang_dict = {k: v for k, v in zip(lang_dict.keys(), map(lambda x: x.text, results))}
-json.dump(ko_lang_dict, open(os.path.join(OUTPUT_LANG_PATH, "ko_kr.json"), "w"), indent=4, ensure_ascii=False)
+def convert_quests() -> None:
+    for file_path in Path(OUTPUT_QUEST_PATH).glob('**/*.snbt'):
+        with open(file_path, "r", encoding="utf-8-sig") as f:
+            snbt = f.read()
+        chapter = file_path.stem.replace(" ", "_")
+        snbt = convert_snbt(snbt, modpack=MODPACK_NAME, chapter=chapter)
+        with open(file_path, "w", encoding="utf-8-sig") as f:
+            f.write(snbt)
+
+if __name__ == "__main__":
+    copy_quests()
+    convert_quests()
+    save_lang(lang_dict, "en_us")
+
+    #ko_lang_dict = translate_lang(lang_dict, "ko")
+    #save_lang(ko_lang_dict, "ko_kr")
