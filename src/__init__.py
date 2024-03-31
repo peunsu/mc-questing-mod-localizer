0e6ecdaddf393abc0bd8894d998b6c76be84af70
peunsu <44690893+peunsu@users.noreply.github.com>
2024-03-31 10:23:36 +0000

    Updated ftb-snbt-lib to 0.1.2. Added a version badge to the Home page.

--- src/__init__.py
+++ src/__init__.py
@@ -10,4 +10,4 @@ __all__ = [
     "utils"
 ]
 
-__version__ = "1.5.1"
+__version__ = "1.5.2"
--- src/utils.py
+++ src/utils.py
@@ -24,11 +24,11 @@ def set_page_config(title: str, icon: str) -> None:
             "About": '''
             ### Minecraft Questing Mod Localizer\n
             [Minecraft Questing Mod Localizer](https://github.com/peunsu/mc-questing-mod-localizer) is a web application that helps you to localize quest files of Minecraft questing mods.\n
-            ![GitHub Release](https://img.shields.io/github/v/release/peunsu/mc-questing-mod-localizer?style=for-the-badge)\n
+            [![GitHub Release](https://img.shields.io/github/v/release/peunsu/mc-questing-mod-localizer?style=for-the-badge)](https://github.com/peunsu/mc-questing-mod-localizer/releases/latest)\n
             **ⓒ [peunsu](https://github.com/peunsu)**.\n
             ### Credits\n
-            * **[FTB Quests](https://www.curseforge.com/minecraft/mc-mods/ftb-quests-forge) ⓒ [FTB Team](https://www.curseforge.com/members/ftb).**\n
-            * **[Better Questing](https://www.curseforge.com/minecraft/mc-mods/better-questing) ⓒ [Funwayguy](https://www.curseforge.com/members/funwayguy).**\n
+            * **[FTB Quests](https://www.curseforge.com/minecraft/mc-mods/ftb-quests-forge) by [FTB Team](https://www.curseforge.com/members/ftb).**\n
+            * **[Better Questing](https://www.curseforge.com/minecraft/mc-mods/better-questing) by [Funwayguy](https://www.curseforge.com/members/funwayguy).**\n
             '''
         }
     )
